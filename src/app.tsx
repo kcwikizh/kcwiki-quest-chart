970385e433c1d4c3b301de755e4c4e66e1c05727
lawvs <lawvs@users.noreply.github.com>
2019-09-15 05:37:21 +0900

    feat: add chart

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import styled from 'styled-components'
+
 import { GlobalStyle } from './styles'
+import { chart } from './chart'
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+`
+
+const Wrapper = styled.div`
+  flex: 1;
+  line-height: 1.5;
+  display: flex;
+  flex-direction: column;
+`
+
+const Chart = styled.div`
+  width: 100%;
+  height: 100%;
+
+  .nodetext {
+    font-family: SimSun;
+    font-size: 12px;
+    fill: #000;
+    user-select: none;
+  }
+`
 
 export const App = () => {
+  const chartRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const drawChart = () => {
+      if (!chartRef.current) {
+        return
+      }
+
+      const node: HTMLElement = chart({
+        width: chartRef.current.clientWidth,
+        height: chartRef.current.clientHeight,
+      })
+      chartRef.current.appendChild(node)
+    }
+    drawChart()
+  }, [chartRef])
+
   return (
     <>
       <GlobalStyle />
-      <div>Hello</div>
+      <Container>
+        <Wrapper>
+          <Chart ref={chartRef}></Chart>
+        </Wrapper>
+      </Container>
     </>
   )
 }
---
+++ src/chart.js
@@ -0,0 +1,113 @@
+import * as d3 from 'd3'
+import questNodes from '../data/poi.json'
+import questLinks from '../data/edges.json'
+
+const drag = simulation => {
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+    d.fx = d.x
+    d.fy = d.y
+  }
+
+  function dragged(d) {
+    d.fx = d3.event.x
+    d.fy = d3.event.y
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0)
+    d.fx = null
+    d.fy = null
+  }
+
+  return d3
+    .drag()
+    .on('start', dragstarted)
+    .on('drag', dragged)
+    .on('end', dragended)
+}
+
+const color = type => {
+  // @see https://github.com/kcwikizh/kcdata/wiki/Quest
+  const defaultColor = '#111'
+  const colorMap = {
+    1: 'rgb(152, 217, 142)',
+    2: 'rgb(255, 151, 151)',
+    3: 'rgb(152, 217, 142)',
+    4: 'rgb(166, 255, 255)',
+    5: 'rgb(215, 207, 58)',
+    6: 'rgb(168, 111, 76)',
+    7: 'rgb(201, 165, 226)',
+    8: 'rgb(255, 151, 151)',
+    9: '#222',
+  }
+  if (!colorMap[type]) {
+    return defaultColor
+  }
+  return colorMap[type]
+}
+
+const chart = ({ width, height }) => {
+  const links = questLinks
+  const nodes = questNodes
+
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force('link', d3.forceLink(links).id(d => d.game_id))
+    .force('charge', d3.forceManyBody())
+    .force('center', d3.forceCenter(width / 2, height / 2))
+
+  const svg = d3.create('svg').attr('viewBox', [0, 0, width, height])
+
+  const link = svg
+    .append('g')
+    .attr('stroke', '#999')
+    .attr('stroke-opacity', 0.6)
+    .selectAll('line')
+    .data(links)
+    .join('line')
+    .attr('stroke-width', d => Math.sqrt(d.value))
+
+  const node = svg
+    .append('g')
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 1.5)
+    .selectAll('circle')
+    .data(nodes)
+    .join('circle')
+    .attr('r', 10)
+    .attr('fill', d => color(d.category))
+    .call(drag(simulation))
+
+  const text_dx = -20
+  const text_dy = 20
+
+  const nodesText = svg
+    .selectAll('.nodetext')
+    .data(nodes)
+    .enter()
+    .append('text')
+    .attr('class', 'nodetext')
+    .attr('dx', text_dx)
+    .attr('dy', text_dy)
+    .text(d => d.name)
+
+  node.append('title').text(d => d.name)
+
+  simulation.on('tick', () => {
+    link
+      .attr('x1', d => d.source.x)
+      .attr('y1', d => d.source.y)
+      .attr('x2', d => d.target.x)
+      .attr('y2', d => d.target.y)
+
+    node.attr('cx', d => d.x).attr('cy', d => d.y)
+
+    nodesText.attr('x', d => d.x)
+    nodesText.attr('y', d => d.y)
+  })
+
+  return svg.node()
+}
+
+export { chart }
